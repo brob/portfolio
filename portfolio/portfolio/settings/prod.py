2bce576da7fa91dc9252a3ace767c1dbb6801871
Bryan Robinson <bryanlrobinson@gmail.com>
2013-06-06 20:45:58 -0500

    Media changes

--- portfolio/portfolio/settings/prod.py
+++ portfolio/portfolio/settings/prod.py
@@ -20,12 +20,12 @@ ALLOWED_HOSTS = []
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/home/bryanlrobinson/webapps/static_port/static-port/media/'
+MEDIA_ROOT = '/home/bryanlrobinson/webapps/static_port/static-port/media/portfolio/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = 'http://media.bryanlrobinson.com/'
+MEDIA_URL = 'http://media.bryanlrobinson.com/media/portfolio/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
