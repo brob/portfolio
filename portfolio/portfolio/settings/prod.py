9c25ca232a3e9652d171dd09f1252f0b80c99959
Bryan Robinson <bryanlrobinson@gmail.com>
2013-06-06 20:50:40 -0500

    bad uri

--- portfolio/portfolio/settings/prod.py
+++ portfolio/portfolio/settings/prod.py
@@ -20,7 +20,7 @@ ALLOWED_HOSTS = []
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/home/bryanlrobinson/webapps/static_port/static-port/media/portfolio/'
+MEDIA_ROOT = '/home/bryanlrobinson/webapps/static_port/media/portfolio/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
