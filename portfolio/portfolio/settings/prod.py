0c2ea7cdc297a22238684e085f9279f9c63483c3
Bryan Robinson <bryanlrobinson@gmail.com>
2013-07-01 14:55:38 -0500

    Changed static root

--- portfolio/portfolio/settings/prod.py
+++ portfolio/portfolio/settings/prod.py
@@ -31,7 +31,7 @@ MEDIA_URL = 'http://media.bryanlrobinson.com/media/portfolio-media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/bryanlrobinson/webapps/static_port/media/portfolio-collection/'
+STATIC_ROOT = '/home/bryanlrobinson/webapps/static_port/media/portfolio-static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
